b4ca0e08b3aa7a99eec9ac5555209c66614cb8a7
Andreas Sahle <andreas.sahle@gmail.com>
2020-03-18 17:33:20 +0100

    feat: refactor page and container to theme-ui

--- src/components/Layout/Container.tsx
+++ src/components/Layout/Container.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
-import styled from '@emotion/styled';
-import { Box } from 'prestyled';
+/** @jsx jsx */
+import { jsx, Box } from 'theme-ui';
 
-export const Container = styled(Box)``;
-
-Container.defaultProps = {
-  maxWidth: ['100%', '45rem'],
-  mx: 'auto',
-  px: [2, 0],
-};
+export const Container = () => (
+  <Box
+    mx="auto"
+    px={[2, 0]}
+    sx={{
+      maxWidth: ['100%', '45rem'],
+    }}
+  />
+);
--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,38 +1,27 @@
-import * as React from 'react';
-import styled, { StyledComponent } from '@emotion/styled';
-import { Theme, Box } from 'prestyled';
+/** @jsx jsx */
+import { jsx, Box } from 'theme-ui';
 
-export const Page: StyledComponent<
-  React.PropsWithoutRef<JSX.IntrinsicElements['div']>,
-  { sidebarVisible: boolean },
-  Theme
-> = styled(Box)`
-  background: #fff;
-  overflow: hidden;
-  transition: transform 0.25s ease-in-out;
-  min-height: 100vh;
-  ${props =>
-    !props.sidebarVisible
-      ? `
-    pointer-events: auto;
-    position: relative;
-    z-index: 1;
-  `
-      : `
-    pointer-events: none;
-    height: 100%;
-    left: 0;
-    pointer-events: none;
-    position: fixed;
-    top: 0;
-    transform: translate(-100%,0);
-    transform: translate3d(-100%,0,0);
-    width: 100%;
-    z-index: 9998;
-    @media only screen and (min-width: 600px) {
-      transform: translate(-600px,0);
-      transform: translate3d(-600px,0,0);
-      box-shadow: 0 1px 13px #000;
-    }
-  `};
-`;
+export const Page = ({ sidebarVisible }: { sidebarVisible: boolean }) => (
+  <Box
+    bg="white"
+    sx={{
+      overflow: 'hidden',
+      transition: 'transform 0.25s ease-in-out',
+      minHeight: '100vh',
+      height: '100%',
+      width: '100%',
+      pointerEvents: !sidebarVisible ? 'auto' : 'none',
+      position: !sidebarVisible ? 'relative' : 'fixed',
+      zIndex: !sidebarVisible ? '1' : '9998',
+      ...(sidebarVisible && {
+        left: 0,
+        top: 0,
+        transform: 'translate3d(-100%,0,0)',
+        '@media only screen and (min-width: 600px)': {
+          transform: 'translate3d(-600px, 0, 0)',
+          boxShadow: '0 1px 13px #000',
+        },
+      }),
+    }}
+  />
+);
