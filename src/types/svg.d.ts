9c5edecb1a89d973978d734abf842bdb847c2693
Andreas Adam <andreas.sahle@gmail.com>
2020-04-07 12:48:06 +0200

    chore: improve svg definition

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -1,5 +1,6 @@
 declare module '*.svg' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const content: any;
-  export default content;
+  import React = require('react');
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
 }
