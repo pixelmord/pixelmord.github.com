bb65db311b13a3d57dc47fc8432ecd7f4afe2125
Andreas Adam <andreas.sahle@gmail.com>
2020-06-10 18:56:12 +0200

    refactor: remove gatsby specific files

--- gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ThemeProvider } from 'emotion-theming';
-import { theme } from './src/styles/theme';
-
-export const wrapRootElement = ({ element }) => {
-  return <ThemeProvider theme={theme}>{element}</ThemeProvider>;
-};
